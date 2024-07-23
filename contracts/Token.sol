729f8f4d583fa78b6fde75bd46c29de9a09c7480
rikindrn <95050311+rikindrn@users.noreply.github.com>
2024-07-24 01:59:54 +0700

    first commit

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("EggToken","EGG"){} 
+    constructor()ERC20("IzzyToken","IZZY"){} 
 
     function mint1000tokens() public {
         _mint(msg.sender,1000*10**18);
